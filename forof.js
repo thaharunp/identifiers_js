e4b6abc8b37405dd82c45a3f89fb036ab016ee0d
sadhakunps <sadaqunps@gmail.com>
2024-06-06 11:40:24 +0530

    extra

---
+++ forof.js
@@ -0,0 +1,14 @@
+let employee = [{id:101 , ename:"thaharun", sal:4.5},
+            {id:102 , ename:"thiba", sal:4.6},
+           { id:103 , ename:"nasir", sal:5.6} 
+]
+   /* for(eid of eids){
+    console.log(~ eids id: ${eids.id} and eids ename:${eids.ename}~)
+    } */
+    let i=0
+    while (i<=employee.lengh-1) {
+        console.log(~ employee id: ${employee[i].id} and employee ename : ${employee[i].ename})
+        i++
+    }
+ 
+        
